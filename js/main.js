9520abce9597bb8f31e4136101ed239acf7fcb4a
domstrueboy <domstrueboy@yandex.ru>
2018-05-09 09:36:15 +0500

    a first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+(function(){
+// DOM-nodes:
+const field = document.querySelector('.field');
+         
+// Initial values:
+let numberOfRows    = 4,
+    numberOfColumns = 5;
+
+// Заполнение поля клетками
+for (let i = 0; i < numberOfRows; i++) {
+    let row = document.createElement("div");
+    row.classList.add('field__row', `row${i}`);
+    field.appendChild(row);
+    for (let j = 0; j < numberOfColumns; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add('cell', `column${j}`);
+        row.appendChild(cell);
+
+        //cell.style.height = cell.style.width;
+
+        let cellContent = document.createElement("div");
+        cellContent.classList.add('cell__content');
+        cell.appendChild(cellContent);
+    }
+}
+
+//let x0, y0;
+//let x = 0, y = 0;
+//let card;
+
+// Events:
+//items.forEach(item => {
+//    item.addEventListener('mousedown', down);
+//});
+//window.addEventListener('mousemove', move);
+
+// Functions:
+/*function down(e) {
+    if (!e.target.matches('.item')) { return; }
+    e.preventDefault();
+
+    isHandling = true;
+    x = 0, y = 0;
+    card = e.target;
+    x0 = card.offsetLeft; y0 = card.offsetTop;
+    let width = card.clientWidth;
+
+    e.target.style.opacity = 0.3;
+
+    shuttle.style.display = 'block';
+    shuttle.style.left = `${x0}px`;
+    shuttle.style.top = `${y0}px`;
+    shuttle.style.width = `${width}px`;
+
+    shuttle.addEventListener('mouseup', up);
+    shuttle.addEventListener('mouseleave', up);
+    console.log(e);
+}
+
+function up(e) {
+    if (!e.target.matches('.item')) { return; }
+    e.preventDefault();
+
+    isHandling = false;
+    shuttle.style.display = 'none';
+
+    if (e.x > document.body.clientWidth - colLast.clientWidth && x0 < colFirst.clientWidth) {
+        colFirst.removeChild(card);
+        ( e.relatedTarget === null ) ? colLast.appendChild(card) : colLast.insertBefore(card, e.relatedTarget);
+    }
+    if (e.x < colFirst.clientWidth && x0 > document.body.clientWidth - colLast.clientWidth) {
+        colLast.removeChild(card);
+        ( e.relatedTarget === null ) ? colFirst.appendChild(card) : colFirst.insertBefore(card, e.relatedTarget);
+    }
+    
+    card.style.opacity = 1;
+
+    shuttle.removeEventListener('mouseup', up);
+    shuttle.removeEventListener('mouseleave', up);
+    console.log(e);
+}
+
+function move(e) {
+    if (!isHandling) { return; }
+    e.preventDefault();
+    
+    x += e.movementX; y += e.movementY;
+    shuttle.style.transform = `translate(${x}px, ${y}px) rotate(15deg)`;
+}*/
+})();
